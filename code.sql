bf2ef3ab8f35c5a3006a12ea467f11c72f4665c6
Eric Garcia <eggarcia2891@gmail.com>
2018-07-13 17:49:22 -0500

    Update code.sql

--- code.sql
+++ code.sql
@@ -3,7 +3,7 @@ SELECT question, COUNT(DISTINCT user_id)
 FROM survey
 GROUP BY 1;
 
--- # of people that answered that "Not Sure."\
+-- # of people that answered that "Not Sure."
 SELECT COUNT(*) 
 FROM survey
 WHERE question = '5. When was your last eye exam?'
@@ -31,7 +31,7 @@ FROM funnels;
 WITH funnels AS (
   SELECT DISTINCT q.user_id,
 	h.user_id IS NOT NULL AS 'home_try_on',
-  h.number_of_pairs,\
+  h.number_of_pairs,
   p.user_id IS NOT NULL AS 'is_purchase'
 FROM quiz q
 LEFT JOIN home_try_on h
